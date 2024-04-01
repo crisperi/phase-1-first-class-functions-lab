41833037359a90a0308ec326bd16d71b8f71411c
crisperi <phoenixedian@gmail.com>
2024-04-01 13:17:57 +0300

    added some functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = drivers => {
+    const lastTwo = drivers.slice(-2);
+    return lastTwo;
+}  
+
+const selectingDrivers = (drivers) =>{
+returnFirstTwoDrivers(drivers);
+returnLastTwoDrivers(drivers);
+}
